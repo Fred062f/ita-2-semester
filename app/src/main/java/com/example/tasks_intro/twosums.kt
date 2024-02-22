15925a4275aaae2d79251e855f61ef0def036563
Frederik <frederik.damsgaard01@gmail.com>
2024-02-22 11:35:19 +0100

    TwoSum Assignment changed

--- app/src/main/java/com/example/tasks_intro/twosums.kt
+++ app/src/main/java/com/example/tasks_intro/twosums.kt
@@ -4,20 +4,6 @@ fun main() {
     println(twoSum(intArrayOf(3, 3), 6))
 }
 
-/*
-fun twoSum(nums: IntArray, target: Int) {
-    for (i in nums) {
-        for (j in nums) {
-            if (i + j == target) {
-                println("${nums.indexOf(i)}, ${nums.indexOf(j)}")
-                break
-            }
-        }
-    }
-}
-
- */
-
 fun twoSum(nums: IntArray, target: Int): String {
     for (i in nums.indices) {
         for (j in nums.indices) {
