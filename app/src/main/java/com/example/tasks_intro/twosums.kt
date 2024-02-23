3f9613e135be4e1c233fb94a449700a428309d1e
Frederik <frederik.damsgaard01@gmail.com>
2024-02-23 14:18:43 +0100

    TwoSum Function Modified

--- app/src/main/java/com/example/tasks_intro/twosums.kt
+++ app/src/main/java/com/example/tasks_intro/twosums.kt
@@ -1,9 +1,10 @@
 package com.example.tasks_intro
 
 fun main() {
-    println(twoSum(intArrayOf(3, 3), 6))
+    println(twoSum(intArrayOf(3, 2, 3), 6))
 }
 
+/*
 fun twoSum(nums: IntArray, target: Int): String {
     for (i in nums.indices) {
         for (j in nums.indices) {
@@ -15,4 +16,18 @@ fun twoSum(nums: IntArray, target: Int): String {
         }
     }
     return "False"
+}
+
+ */
+
+fun twoSum(nums: IntArray, target: Int): Pair<Any?, Int> {
+    var hashMap: HashMap<Int, Int> = hashMapOf()
+    for (i in nums.indices) {
+        if ((target - nums[i]) in hashMap.keys) {
+            return Pair(hashMap[target - nums[i]], i)
+        } else {
+            hashMap[nums[i]] = i
+        }
+    }
+    return Pair(-1, -1)
 }
